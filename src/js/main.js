b202aa7072ee4e807fed4d10987446d285cd1211
hans <leekorea09@gmail.com>
2018-06-11 19:00:53 +0900

    accessToken added

--- src/js/main.js
+++ src/js/main.js
@@ -2,21 +2,23 @@ const express = require('express');
 import Instagram from 'node-instagram';
 
 // Create a new instance.
-const instagram = new Instagram({
+let instagram = new Instagram({
   clientId: process.env.NODE_ENV === 'sandbox' ? require('../../config.json').clientId : process.env.CLIENTID,
-  clientSecret: process.env.NODE_ENV === 'sandbox' ? require('../../config.json').clientSecret : process.env.CLIENTSECRET
+  clientSecret: process.env.NODE_ENV === 'sandbox' ? require('../../config.json').clientSecret : process.env.CLIENTSECRET,
+  accessToken: process.env.NODE_ENV === 'sandbox' ? '' : process.env.accessToken
 });
 
-const redirectUri = process.env.REDIRECT_URI + '/auth/instagram/callback';
+const redirectUri = (process.env.NODE_ENV === 'sandbox' ?  'http://localhost:8080' : process.env.REDIRECT_URI)+ '/auth/instagram/callback';
 
 const port = process.env.PORT || 8080;
 
 // create express server
 const app = express();
 
-// Redirect user to instagram oauth
-app.get('/auth/instagram', (req, res) => {
-  res.redirect(
+console.log('instagram: ', instagram, process.env.NODE_ENV);
+
+const getAuth = async function(res) {
+  return res.redirect(
     instagram.getAuthorizationUrl(
       redirectUri,
       {
@@ -24,7 +26,11 @@ app.get('/auth/instagram', (req, res) => {
         scope: ['basic', 'public_content'],
       }
     )
-  );
+  )
+}
+// Redirect user to instagram oauth
+app.get('/auth/instagram', (req, res) => {
+    getAuth(res);
 });
 
 // Handle auth code and get access_token for user
@@ -34,19 +40,18 @@ app.get('/auth/instagram/callback', async (req, res) => {
     const code = req.query.code;
     const data = await instagram.authorizeUser(code, redirectUri);
     // data.access_token contain the user access_token
+    instagram.config.accessToken = data.access_token;
     res.json(data);
   } catch (err) {
     res.json(err);
   }
 });
 
-app.get('/tag/:tag', (req, res) => {
-  // Get information about a tag object.
-  console.log(instagram);
-
-  instagram.get(`tags/${req.params.tag}`).then(data => {
+app.get('/tags/:tag', (req, res) => {
+  instagram.get(`tags/${req.params.tag}`, {access_token: instagram.config.accessToken}).then(data => {
     console.log(data);
-  })
+    res.json(data);
+  });
 });
 
 // listen to port 3000
